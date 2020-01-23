859fb03bbcb85b24609268281622c911dd3f6bd8
icedbearx <59765638+icedbearx@users.noreply.github.com>
2020-01-23 14:02:30 +0700

    Add files via upload

---
+++ print.java
@@ -0,0 +1,11 @@
+package app;
+
+public class print {
+    public static void main(String[] args) throws Exception {
+    System.out.println("Student ID : 552115099");
+    System.out.println("Registration Result");
+    System.out.println("Semester 1 / 2559");
+    System.out.println("No."+"  "+"Course No."+"       "+"Course Title"+"       "+"Credit"+"  "+"Grade");
+    System.out.println(Order+)
+ }
+}
