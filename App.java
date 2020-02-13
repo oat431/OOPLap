3da14b5b2d68154448f40ad2c685a43f6874f400
icedbearx <59765638+icedbearx@users.noreply.github.com>
2020-02-13 14:42:10 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,7 +1,46 @@
 package app;
-import java.io.*;
+
+//62 21 15 039
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App {
-    public static void main(String[] args) throws FileNotFoundException {
-        Regitration student = new Regitration();
+    public String getStudentID() {
+        return studentId;
     }
+
+    private String studentId;
+
+    public static void main(String args[]) {
+        App app = new App();
+        app.setStudentId("622115039");
+    }
+
+    public void setStudentId(String studentId) {
+
+        if (!studentId.matches("[0-9]+")) {
+            System.out.println("Invalid");
+            return;
+        }
+
+        if (studentId.length() != 9) {
+            System.out.println("Invalid");
+            return;
+        }
+
+        String year = studentId.substring(0, 2);
+        String faculty = studentId.substring(2, 4);
+        String type = studentId.substring(4, 7);
+        int b = Integer.valueOf(year);
+        int n = Integer.valueOf(faculty);
+
+        if (b > 62) {
+            System.out.println("Invalid");
+        } else if (n > 21) {
+            System.out.println("Invalid");
+        } else {
+            this.studentId = studentId;
+        }
+    }
+    
 }
